f7d9e3e07f6bf8ec9a4ec63450d23169dd545bb5
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-01-18 14:44:56 +0300

    Add: User account messages

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,89 @@
+import React, {useState, useEffect, memo, useCallback} from 'react'
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import Moment from 'react-moment'
+import 'moment/locale/ru'
+import {convertToLocaleDate} from '../helpers/convertToLocaleDate'
+import ImageViewer from 'react-simple-image-viewer'
+import {getImageURL} from '../helpers/image'
+
+const ChatMessage = ({avatarUser, keyArr, arr}) => {
+    const user = useSelector((state) => state.auth.user)
+
+    const newDate = keyArr && convertToLocaleDate(keyArr, true)
+    const convertedDate = new Date(newDate)
+    const [currentImage, setCurrentImage] = useState(null)
+    const [isViewerOpen, setIsViewerOpen] = useState(false)
+    const [images, setImages] = useState([])
+
+    const openImageViewer = useCallback((index) => {
+        setCurrentImage(index)
+        setIsViewerOpen(true)
+    }, [])
+
+    const closeImageViewer = () => {
+        setCurrentImage(0)
+        setIsViewerOpen(false)
+    }
+
+    useEffect(() => {
+        setImages(
+            arr
+                ?.map((i) => i.medias?.map((k) => getImageURL(k.media)))
+                .flat()
+                .reverse()
+        )
+    }, [arr])
+
+    return (
+        <>
+            <Moment locale="ru" format="DD MMMM" date={convertedDate} />
+            {arr &&
+                arr.map((i) => (
+                    <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}`} key={i?.id}>
+                        <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}-user`}>
+                            <img
+                                src={getImageURL(user?.id === i?.userId ? user?.avatar : avatarUser)}
+                                alt="avatar"
+                            />
+                        </div>
+
+                        <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}-messages`}>
+                            <div className="bubble" style={!i.isViewed ? {border: 'thin solid red'} : undefined}>
+                                <p>{i?.text}</p>
+                                <div className="images-message">
+                                    {i.medias?.length > 0
+                                        ? i.medias.map((k, index) => (
+                                              <div
+                                                  key={k.id}
+                                                  className="images-box"
+                                                  onClick={() => {
+                                                      openImageViewer(index)
+                                                  }}
+                                              >
+                                                  <img src={getImageURL(k.media)} height={'100%'} width={'100%'} />
+                                              </div>
+                                          ))
+                                        : ''}
+                                </div>
+                                {isViewerOpen && (
+                                    <ImageViewer
+                                        src={images}
+                                        currentIndex={currentImage}
+                                        onClose={closeImageViewer}
+                                        disableScroll={false}
+                                        backgroundStyle={{
+                                            backgroundColor: 'rgba(0,0,0,0.9)',
+                                        }}
+                                        closeOnClickOutside={true}
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+        </>
+    )
+}
+
+export default memo(ChatMessage)
--- src/pages/account/Messages.jsx
+++ src/pages/account/Messages.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+// import {useSelector} from 'react-redux'
 import MessagePreview from '../../components/MessagePreview'
 import {Link} from 'react-router-dom'
 import {FiArrowLeft} from 'react-icons/fi'
 
+// import useSocketConnect from '../../hooks/socketConnect'
+import {emitPaginateConversation} from '../../services/sockets/conversations'
+// import {setSocketConnection} from '../../services/sockets/socketInstance'
+
 const Messages = () => {
+    // const user = useSelector((state) => state?.auth?.user)
+    // const {isConnected} = useSocketConnect()
+    // const [currentPage, setCurrentPage] = useState(1)
+    const [conversations, setConversations] = useState()
+
+    useEffect(() => {
+        emitPaginateConversation({page: 1})
+            .then((res) => (res.status === 200 ? setConversations(res.body.data) : console.log(res)))
+            .catch((e) => console.log(e))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return (
         <div className="main p-0 py-3 py-sm-4">
             <div className="d-flex align-items-center mb-3 mb-sm-4 ms-3 ms-sm-4 ms-lg-5">
@@ -14,10 +31,11 @@ const Messages = () => {
             </div>
 
             <ul className="messages-list">
-                <MessagePreview />
-                <MessagePreview />
-                <MessagePreview />
-                <MessagePreview />
+                {conversations &&
+                    conversations.map((conversation) => {
+                        console.log(conversation)
+                        return <MessagePreview key={conversation.id} conversation={conversation} />
+                    })}
             </ul>
         </div>
     )
