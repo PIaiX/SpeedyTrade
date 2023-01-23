ee7a055db17a0e94ae22e6943280a44e1c5feb4d
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-01-23 17:38:21 +0300

    Add: Image viewer

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -7,14 +7,11 @@ import {convertToLocaleDate} from '../helpers/convertToLocaleDate'
 import ImageViewer from 'react-simple-image-viewer'
 import {getImageURL} from '../helpers/image'
 
-const ChatMessage = ({avatarUser, keyArr, arr}) => {
+const SingleMessage = ({msg}) => {
     const user = useSelector((state) => state.auth.user)
-
-    const newDate = keyArr && convertToLocaleDate(keyArr, true)
-    const convertedDate = new Date(newDate)
     const [currentImage, setCurrentImage] = useState(null)
     const [isViewerOpen, setIsViewerOpen] = useState(false)
-    const [images, setImages] = useState([])
+    const [image, setImage] = useState([])
 
     const openImageViewer = useCallback((index) => {
         setCurrentImage(index)
@@ -27,63 +24,75 @@ const ChatMessage = ({avatarUser, keyArr, arr}) => {
     }
 
     useEffect(() => {
-        setImages(
-            arr
-                ?.map((i) => i.medias?.map((k) => getImageURL(k.media)))
-                .flat()
-                .reverse()
-        )
-    }, [arr])
+        const loadImage = async (url) => {
+            return new Promise((res, rej) => {
+                let img = new Image()
+                img.onload = () => {
+                    res(img.src)
+                }
+                img.onerror = (e) => {
+                    setTimeout(() => {
+                        loadImage(url).then((i) => res(i))
+                    }, 4000)
+                }
+                img.src = url
+            })
+        }
+        msg.attachedfile &&
+            loadImage(getImageURL(msg.attachedfile))
+                .then((img) => setImage([img]))
+                .catch((e) => console.log(e))
+    }, [msg])
+
+    return (
+        <div className={`chat-box${user?.id === msg?.userId ? '-reverse' : ''}`}>
+            <div className={`chat-box${user?.id === msg?.userId ? '-reverse' : ''}-user`}>
+                <img src={getImageURL(msg?.userAvatar)} alt="avatar" />
+            </div>
+
+            <div className={`chat-box${user?.id === msg?.userId ? '-reverse' : ''}-messages`}>
+                <div className="bubble" style={!msg.isViewed ? {border: 'thin solid var(--bg-2)'} : undefined}>
+                    <p>{msg?.text}</p>
+                    <div className="images-message">
+                        {msg.attachedfile ? (
+                            <div
+                                className="images-box"
+                                onClick={() => {
+                                    openImageViewer(0)
+                                }}
+                            >
+                                {image.length !== 0 && <img src={image} height={'100%'} width={'100%'} />}
+                            </div>
+                        ) : (
+                            ''
+                        )}
+                    </div>
+                    {isViewerOpen && (
+                        <ImageViewer
+                            src={image}
+                            currentIndex={currentImage}
+                            onClose={closeImageViewer}
+                            disableScroll={false}
+                            backgroundStyle={{
+                                backgroundColor: 'rgba(0,0,0,0.9)',
+                            }}
+                            closeOnClickOutside={true}
+                        />
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const ChatMessage = ({avatarUser, keyArr, arr}) => {
+    const newDate = keyArr && convertToLocaleDate(keyArr, true)
+    const convertedDate = new Date(newDate)
 
     return (
         <>
             <Moment locale="ru" format="DD MMMM" date={convertedDate} />
-            {arr &&
-                arr.map((i) => (
-                    <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}`} key={i?.id}>
-                        <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}-user`}>
-                            <img
-                                src={getImageURL(user?.id === i?.userId ? user?.avatar : avatarUser)}
-                                alt="avatar"
-                            />
-                        </div>
-
-                        <div className={`chat-box${user?.id === i?.userId ? '-reverse' : ''}-messages`}>
-                            <div
-                                className="bubble"
-                                style={!i.isViewed ? {border: 'thin solid var(--bg-2)'} : undefined}
-                            >
-                                <p>{i?.text}</p>
-                                <div className="images-message">
-                                    {i.attachedfile ? (
-                                        <div
-                                            className="images-box"
-                                            onClick={() => {
-                                                openImageViewer(0)
-                                            }}
-                                        >
-                                            <img src={getImageURL(i.attachedfile)} height={'100%'} width={'100%'} />
-                                        </div>
-                                    ) : (
-                                        ''
-                                    )}
-                                </div>
-                                {isViewerOpen && (
-                                    <ImageViewer
-                                        src={images}
-                                        currentIndex={currentImage}
-                                        onClose={closeImageViewer}
-                                        disableScroll={false}
-                                        backgroundStyle={{
-                                            backgroundColor: 'rgba(0,0,0,0.9)',
-                                        }}
-                                        closeOnClickOutside={true}
-                                    />
-                                )}
-                            </div>
-                        </div>
-                    </div>
-                ))}
+            {arr && arr.map((i) => <SingleMessage msg={i} ava={avatarUser} key={`chat${i.id}`} />)}
         </>
     )
 }
