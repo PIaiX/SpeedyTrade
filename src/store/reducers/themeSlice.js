2ca307249eff8301e15290698c15bd9abb380c2a
Ramil Zinnatullin <ramil.zinnatullin.02@mail.ru>
2023-01-26 13:46:02 +0300

    Fix: light and dark sides

--- src/store/reducers/themeSlice.js
+++ src/store/reducers/themeSlice.js
@@ -9,15 +9,31 @@ const themeSlice = createSlice({
     initialState,
     reducers: {
         initTheme: (state) => {
-            document.documentElement.dataset.theme = state.mode
+            const faviconNode = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+
+            faviconNode.forEach(function(element) {
+                element.setAttribute('href', '/favicon-light.svg');
+            });
+
+            document.documentElement.dataset.theme = 'light'
+
         },
         switchTheme: (state) => {
-            const isLight = state.mode === 'light'
-            const faviconNode = document.getElementById('favicon')
 
-            faviconNode.href = isLight ? 'favicon-dark.svg' : 'favicon-light.svg'
+            const isLight = state.mode === 'light'
+            const faviconNode = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+
+            let img;
+            faviconNode.href = isLight ? img='favicon-dark.svg' : img='favicon-light.svg'
+
+            faviconNode.forEach(function(element) {
+                element.setAttribute('href', '/'+img);
+            });
+
             state.mode = isLight ? 'dark' : 'light'
             document.documentElement.dataset.theme = isLight ? 'dark' : 'light'
+
+
         },
     },
 })
