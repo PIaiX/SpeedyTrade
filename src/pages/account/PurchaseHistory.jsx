46bdc58aa0ae4d16dc23660957cf0997dd7335e6
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-02-16 13:28:31 +0300

    Remove: Skeletons

--- src/pages/account/PurchaseHistory.jsx
+++ src/pages/account/PurchaseHistory.jsx
@@ -1,13 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table'
 import AdsTr3 from '../../components/AdsTr3'
-import {Link} from 'react-router-dom'
-import {FiArrowLeft} from 'react-icons/fi'
-import {getPurchases} from '../../services/purchases'
-import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import { getPurchases } from '../../services/purchases'
+import { useSelector } from 'react-redux'
 import Paginate from '../../components/utils/paginate'
 import usePagination from '../../hooks/pagination'
-import Skeleton from 'react-loading-skeleton'
 
 const PurchaseHistory = () => {
     const theme = useSelector((state) => state?.theme?.mode)
@@ -19,16 +18,16 @@ const PurchaseHistory = () => {
     })
     const generalLimit = 10
 
-    const {paginationItems, pageCount, selectedPage, setSelectedPage, handlePageClick} = usePagination(
+    const { paginationItems, pageCount, selectedPage, setSelectedPage, handlePageClick } = usePagination(
         purchase?.items,
         generalLimit,
         purchase?.meta?.total
     )
 
     useEffect(() => {
-        getPurchases({page: selectedPage + 1, limit: generalLimit}, userId)
-            .then((res) => setPurchase({isLoaded: true, meta: res?.meta, items: res?.data}))
-            .catch(() => setPurchase({isLoaded: true, meta: null, items: null}))
+        getPurchases({ page: selectedPage + 1, limit: generalLimit }, userId)
+            .then((res) => setPurchase({ isLoaded: true, meta: res?.meta, items: res?.data }))
+            .catch(() => setPurchase({ isLoaded: true, meta: null, items: null }))
     }, [userId, selectedPage])
 
     return (
@@ -40,48 +39,39 @@ const PurchaseHistory = () => {
                 <h4 className="color-1 mb-0">История покупок</h4>
             </div>
 
-            {purchase.isLoaded ? (
-                purchase.meta?.total > 0 ? (
-                    <Table borderless responsive className="my-sm-4">
-                        <thead>
-                            <tr>
-                                <>
-                                    <th>Дата</th>
-                                    <th>Заказ</th>
-                                    <th>Описание</th>
-                                    <th>Продавец</th>
-                                    <th>Статус</th>
-                                    <th>Цена</th>
-                                    <th></th>
-                                </>
-                            </tr>
-                        </thead>
+            {purchase.meta?.total > 0 ? (
+                <Table borderless responsive className="my-sm-4">
+                    <thead>
+                        <tr>
+                            <>
+                                <th>Дата</th>
+                                <th>Заказ</th>
+                                <th>Описание</th>
+                                <th>Продавец</th>
+                                <th>Статус</th>
+                                <th>Цена</th>
+                                <th></th>
+                            </>
+                        </tr>
+                    </thead>
 
-                        <tbody>
-                            {paginationItems?.map((i) => (
-                                <AdsTr3
-                                    key={i?.id}
-                                    lotId={i?.lotId}
-                                    status={i?.statusForUser}
-                                    createdAt={i?.lot?.createdAtForUser}
-                                    description={i?.lot?.description}
-                                    userNickname={i?.lot?.userId}
-                                    price={i?.lot?.price}
-                                    priceCommission={i?.lot?.priceCommission}
-                                />
-                            ))}
-                        </tbody>
-                    </Table>
-                ) : (
-                    <h6>История пуста</h6>
-                )
+                    <tbody>
+                        {paginationItems?.map((i) => (
+                            <AdsTr3
+                                key={i?.id}
+                                lotId={i?.lotId}
+                                status={i?.statusForUser}
+                                createdAt={i?.lot?.createdAtForUser}
+                                description={i?.lot?.description}
+                                userNickname={i?.lot?.userId}
+                                price={i?.lot?.price}
+                                priceCommission={i?.lot?.priceCommission}
+                            />
+                        ))}
+                    </tbody>
+                </Table>
             ) : (
-                <Skeleton
-                    count={7}
-                    baseColor={theme === 'dark' ? `#322054` : '#f05d66'}
-                    highlightColor={theme === 'dark' ? `#5736db` : '#eb3349'}
-                    height={50}
-                />
+                <h6>История пуста</h6>
             )}
 
             {purchase.isLoaded && paginationItems?.length > 0 && (
--- src/pages/account/SalesHistory.jsx
+++ src/pages/account/SalesHistory.jsx
@@ -1,12 +1,11 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Table from 'react-bootstrap/Table'
 import AdsTr3 from '../../components/AdsTr3'
-import {Link} from 'react-router-dom'
-import {FiArrowLeft} from 'react-icons/fi'
-import {useSelector} from 'react-redux'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+import { useSelector } from 'react-redux'
 import usePagination from '../../hooks/pagination'
-import {getSale} from '../../services/purchases'
-import Skeleton from 'react-loading-skeleton'
+import { getSale } from '../../services/purchases'
 import Paginate from '../../components/utils/paginate'
 
 const SalesHistory = () => {
@@ -19,16 +18,16 @@ const SalesHistory = () => {
     })
     const generalLimit = 10
 
-    const {paginationItems, pageCount, selectedPage, setSelectedPage, handlePageClick} = usePagination(
+    const { paginationItems, pageCount, selectedPage, setSelectedPage, handlePageClick } = usePagination(
         saleHistory?.items,
         generalLimit,
         saleHistory?.meta?.total
     )
 
     useEffect(() => {
-        getSale({page: selectedPage + 1, limit: generalLimit}, userId)
-            .then((res) => setSaleHistory({isLoaded: true, meta: res?.meta, items: res?.data}))
-            .catch(() => setSaleHistory({isLoaded: true, meta: null, items: null}))
+        getSale({ page: selectedPage + 1, limit: generalLimit }, userId)
+            .then((res) => setSaleHistory({ isLoaded: true, meta: res?.meta, items: res?.data }))
+            .catch(() => setSaleHistory({ isLoaded: true, meta: null, items: null }))
     }, [userId, selectedPage])
 
     return (
@@ -40,48 +39,39 @@ const SalesHistory = () => {
                 <h4 className="color-1 mb-0">История продаж</h4>
             </div>
 
-            {saleHistory.isLoaded ? (
-                saleHistory.meta?.total > 0 ? (
-                    <Table borderless responsive className="my-sm-4">
-                        <thead>
-                            <tr>
-                                <>
-                                    <th>Дата</th>
-                                    <th>Заказ</th>
-                                    <th>Описание</th>
-                                    <th>Продавец</th>
-                                    <th>Статус</th>
-                                    <th>Цена</th>
-                                    <th></th>
-                                </>
-                            </tr>
-                        </thead>
+            {saleHistory.meta?.total > 0 ? (
+                <Table borderless responsive className="my-sm-4">
+                    <thead>
+                        <tr>
+                            <>
+                                <th>Дата</th>
+                                <th>Заказ</th>
+                                <th>Описание</th>
+                                <th>Продавец</th>
+                                <th>Статус</th>
+                                <th>Цена</th>
+                                <th></th>
+                            </>
+                        </tr>
+                    </thead>
 
-                        <tbody>
-                            {paginationItems?.map((i) => (
-                                <AdsTr3
-                                    key={i?.id}
-                                    lotId={i?.lotId}
-                                    status={i?.statusForUser}
-                                    createdAt={i?.lot?.createdAtForUser}
-                                    description={i?.lot?.description}
-                                    userNickname={i?.lot?.userId}
-                                    price={i?.lot?.price}
-                                    priceCommission={i?.lot?.priceCommission}
-                                />
-                            ))}
-                        </tbody>
-                    </Table>
-                ) : (
-                    <h6>История пуста</h6>
-                )
+                    <tbody>
+                        {paginationItems?.map((i) => (
+                            <AdsTr3
+                                key={i?.id}
+                                lotId={i?.lotId}
+                                status={i?.statusForUser}
+                                createdAt={i?.lot?.createdAtForUser}
+                                description={i?.lot?.description}
+                                userNickname={i?.lot?.userId}
+                                price={i?.lot?.price}
+                                priceCommission={i?.lot?.priceCommission}
+                            />
+                        ))}
+                    </tbody>
+                </Table>
             ) : (
-                <Skeleton
-                    count={7}
-                    baseColor={theme === 'dark' ? `#322054` : '#f05d66'}
-                    highlightColor={theme === 'dark' ? `#5736db` : '#eb3349'}
-                    height={50}
-                />
+                <h6>История пуста</h6>
             )}
 
             {saleHistory.isLoaded && paginationItems?.length > 0 && (
