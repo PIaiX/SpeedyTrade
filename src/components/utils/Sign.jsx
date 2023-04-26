274597c7cadcf19b4e0f6f8d8edd494375554f32
Rus <61886636+rus-sharafiev@users.noreply.github.com>
2023-04-26 18:25:05 +0300

    Changed: Games.ru to SpeedyTrade.ru

--- src/components/utils/Sign.jsx
+++ src/components/utils/Sign.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sign(props) {
     const currentYear = new Date().getFullYear()
-    return <div className={props.className}>© {currentYear} «Games.ru» Все права защищены</div>
+    return <div className={props.className}>© {currentYear} «SpeedyTrade.ru» Все права защищены</div>
 }
 
 export default Sign
